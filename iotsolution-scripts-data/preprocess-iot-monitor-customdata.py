4311758eaf919780ec0a4afd9085034549b20266
jccorpuz <jessa23corpuz@gmail.com>
2025-03-20 14:26:34 -0400

    Update preprocess-iot-monitor-customdata.py

--- iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
+++ iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
@@ -33,8 +33,6 @@ HTTPADDR='https://'
 
 #############################################################################################################
 #                                      STORE VIPER TOKEN
-# Get the VIPERTOKEN from the file admin.tok - change folder location to admin.tok
-# to your location of admin.tok
 def getparams():
      global VIPERHOST, VIPERPORT, HTTPADDR
      with open(basedir + "/Viper-preprocess/admin.tok", "r") as f:
@@ -48,43 +46,37 @@ def getparams():
           
      return VIPERTOKEN
 
-VIPERTOKEN=getparams()
-if VIPERHOST=="":
+VIPERTOKEN = getparams()
+if VIPERHOST == "":
     print("ERROR: Cannot read viper.txt: VIPERHOST is empty or HPDEHOST is empty")
+
 
 #############################################################################################################
 #                                     CREATE TOPICS IN KAFKA
 
-# Set personal data
 def datasetup(maintopic, preprocesstopic):
-     companyname="OTICS"
-     myname="Sebastian"
-     myemail="Sebastian.Maurice"
-     mylocation="Toronto"
+     companyname = "OTICS"
+     myname = "Sebastian"
+     myemail = "Sebastian.Maurice"
+     mylocation = "Toronto"
 
      # Replication factor for Kafka redundancy
-     replication=1
+     replication = 1
      # Number of partitions for joined topic
-     numpartitions=3
+     numpartitions = 3
      # Enable SSL/TLS communication with Kafka
-     enabletls=1
+     enabletls = 1
      # If brokerhost is empty then this function will use the brokerhost address in your
      # VIPER.ENV in the field 'KAFKA_CONNECT_BOOTSTRAP_SERVERS'
-     brokerhost=''
+     brokerhost = ''
      # If this is -999 then this function uses the port address for Kafka in VIPER.ENV in the
      # field 'KAFKA_CONNECT_BOOTSTRAP_SERVERS'
-     brokerport=-999
+     brokerport = -999
      # If you are using a reverse proxy to reach VIPER then you can put it here - otherwise if
      # empty then no reverse proxy is being used
-     microserviceid=''
+     microserviceid = ''
 
-
-     # Create a main topic that will hold data for several streams i.e. if you have 1000 IoT devices, and each device has 10 fields,
-     # rather than creating 10,000 streams you create ONE main stream to hold 10000 streams, this will drastically reduce Kafka partition
-     # costs
-
-
-     description="TML Use Case"
+     description = "TML Use Case"
 
      # Create the 4 topics in Kafka concurrently - it will return a JSON array
      result = maadstml.vipercreatetopic(VIPERTOKEN, VIPERHOST, VIPERPORT, maintopic, companyname,
@@ -102,7 +94,6 @@ def datasetup(maintopic, preprocesstopic):
          pid = p['ProducerId']
          tn = p['Topic']
 
-     # Create the 4 topics in Kafka concurrently - it will return a JSON array
      result = maadstml.vipercreatetopic(VIPERTOKEN, VIPERHOST, VIPERPORT, preprocesstopic, companyname,
                                         myname, myemail, mylocation, description, enabletls,
                                         brokerhost, brokerport, numpartitions, replication,
@@ -115,41 +106,20 @@ def sendtransactiondata(maintopic, mainproducerid, VIPERPORT, index, preprocesst
     #############################################################################################################
     #                                    PREPROCESS DATA STREAMS
 
-    # Roll back each data stream by 10 percent - change this to a larger number if you want more data
-    # For supervised machine learning you need a minimum of 30 data points in each stream
     maxrows = 500
-    # Go to the last offset of each stream: If lastoffset=500, then this function will rollback the 
-    # streams to offset=500-50=450
     offset = -1
-    # Max wait time for Kafka to respond on milliseconds - you can increase this number if
-    #maintopic to produce the preprocess data to
     topic = maintopic
-    # producerid of the topic
     producerid = mainproducerid
-    # use the host in Viper.env file
     brokerhost = ''
-    # use the port in Viper.env file
     brokerport = -999
-    # if load balancing enter the microsericeid to route the HTTP to a specific machine
     microserviceid = ''
 
-    # You can preprocess with the following functions: MAX, MIN, SUM, AVG, COUNT, DIFF, OUTLIERS
-    # here we will take max values of the arcturus-humidity, we will Diff arcturus-temperature, and average arcturus-Light_Intensity
-    # NOTE: The number of process logic functions MUST match the streams - the operations will be applied in the same order
     preprocessconditions = ''
     
-    # Add a 7000 millisecond maximum delay for VIPER to wait for Kafka to return confirmation message is received and written to topic 
-    delay = 70
-    # USE TLS encryption when sending to Kafka Cloud (GCP/AWS/Azure)
-    enabletls = 1
-    array = 0
-    saveasarray = 1
-    topicid = -999
-    
-    rawdataoutput = 1
-    asynctimeout = 120
-    timedelay = 0
+    # This is where we specify the logic for preprocessing
+    preprocesslogic = 'MIN,MAX,COUNT,VARIANCE,OUTLIERS,ANOMPROB'
 
+    # If your preprocess logic includes certain streams, make sure they are specified here
     jsoncriteria = 'uid=metadata.dsn,filter:allrecords~\
     subtopics=metadata.property_name~\
     values=datapoint.value~\
@@ -164,28 +134,30 @@ def sendtransactiondata(maintopic, mainproducerid, VIPERPORT, index, preprocesst
     streamstojoin = "" 
     identifier = "IoT device performance and failures"
 
-    # if dataage - use:dataage_utcoffset_timetype
-    preprocesslogic = 'MIN,MAX,COUNT,VARIANCE,OUTLIERS,ANOMPROB'
+    pathtotmlattrs = 'oem=n/a,lat=n/a,long=n/a,location=n/a,identifier=n/a'
 
-    pathtotmlattrs = 'oem=n/a,lat=n/a,long=n/a,location=n/a,identifier=n/a'          
-    
     try:
-        print(f"Preprocessing with logic: {preprocesslogic}")  # Debugging line
+        # Print debug info to check the preprocess logic
+        print(f"Sending preprocessing with logic: {preprocesslogic}")
+
+        # Send the data to preprocess
         result = maadstml.viperpreprocesscustomjson(VIPERTOKEN, VIPERHOST, VIPERPORT, topic, producerid, offset,
-                                                    jsoncriteria, rawdataoutput, maxrows, enabletls, delay,
-                                                    brokerhost, brokerport, microserviceid, topicid, streamstojoin,
-                                                    preprocesslogic, preprocessconditions, identifier,
-                                                    preprocesstopic, array, saveasarray, timedelay, asynctimeout,
+                                                    jsoncriteria, 1, maxrows, 1, 70, brokerhost, brokerport,
+                                                    microserviceid, -999, streamstojoin, preprocesslogic,
+                                                    preprocessconditions, identifier, preprocesstopic, 0, 1, 0, 120,
                                                     usemysql, tmlfilepath, pathtotmlattrs)
-        print(f"Preprocessing result: {result}")  # Debugging line
+
+        # Print the result to verify if preprocessing was successful
+        print(f"Preprocessing result: {result}")
         return result
+
     except Exception as e:
-        print("ERROR:", e)
+        print(f"ERROR during preprocessing: {e}")
         return e
      
 
 #############################################################################################################
-#                                     SETUP THE TOPIC DATA STREAMS FOR WALMART EXAMPLE
+# SETUP THE TOPIC DATA STREAMS FOR WALMART EXAMPLE
 
 maintopic = 'iot-mainstream'
 preprocesstopic = 'iot-preprocess'
@@ -194,7 +166,6 @@ print(f"Topic setup complete. Main topic: {maintopic}, Producer ID: {producerid}
 
 async def startviper():
     print("Start Preprocess-iot-monitor-customdata Request:", datetime.datetime.now())
-    print("Starting the data stream processing...")  # Debugging line
     while True:
         try:
             sendtransactiondata(maintopic, producerid, VIPERPORT, -1, preprocesstopic)
