96461041fdd490563d1f65d6f8b78c074e8f3a3d
jccorpuz <jessa23corpuz@gmail.com>
2025-03-20 15:00:15 -0400

    Update preprocess-iot-monitor-customdata.py

--- iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
+++ iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
@@ -11,40 +11,38 @@ import nest_asyncio
 import json
 import random
 from joblib import Parallel, delayed
-#from joblib import parallel_backend
 import sys
 import multiprocessing
 import pandas as pd
-#import concurrent.futures
 import asyncio
-# Uncomment IF using Jupyter notebook
-nest_asyncio.apply()
 import datetime
 import time
 import os
 
-basedir = os.environ['userbasedir'] 
+# Ensure Jupyter compatibility
+nest_asyncio.apply()
 
-# Set Global Host/Port for VIPER - You may change this to fit your configuration
-VIPERHOST=''
-VIPERPORT=''
-HTTPADDR='https://'
+# Global paths and environment variables
+basedir = os.environ['userbasedir']
+VIPERHOST = ''
+VIPERPORT = ''
+HTTPADDR = 'https://'
 
 
 #############################################################################################################
 #                                      STORE VIPER TOKEN
 def getparams():
-     global VIPERHOST, VIPERPORT, HTTPADDR
-     with open(basedir + "/Viper-preprocess/admin.tok", "r") as f:
-        VIPERTOKEN=f.read()
+    global VIPERHOST, VIPERPORT, HTTPADDR
+    with open(basedir + "/Viper-preprocess/admin.tok", "r") as f:
+        VIPERTOKEN = f.read()
 
-     if VIPERHOST=="":
+    if VIPERHOST == "":
         with open(basedir + '/Viper-preprocess/viper.txt', 'r') as f:
-          output = f.read()
-          VIPERHOST = HTTPADDR + output.split(",")[0]
-          VIPERPORT = output.split(",")[1]
-          
-     return VIPERTOKEN
+            output = f.read()
+            VIPERHOST = HTTPADDR + output.split(",")[0]
+            VIPERPORT = output.split(",")[1]
+
+    return VIPERTOKEN
 
 VIPERTOKEN = getparams()
 if VIPERHOST == "":
@@ -55,57 +53,45 @@ if VIPERHOST == "":
 #                                     CREATE TOPICS IN KAFKA
 
 def datasetup(maintopic, preprocesstopic):
-     companyname = "OTICS"
-     myname = "Sebastian"
-     myemail = "Sebastian.Maurice"
-     mylocation = "Toronto"
+    companyname = "OTICS"
+    myname = "Sebastian"
+    myemail = "Sebastian.Maurice"
+    mylocation = "Toronto"
 
-     # Replication factor for Kafka redundancy
-     replication = 1
-     # Number of partitions for joined topic
-     numpartitions = 3
-     # Enable SSL/TLS communication with Kafka
-     enabletls = 1
-     # If brokerhost is empty then this function will use the brokerhost address in your
-     # VIPER.ENV in the field 'KAFKA_CONNECT_BOOTSTRAP_SERVERS'
-     brokerhost = ''
-     # If this is -999 then this function uses the port address for Kafka in VIPER.ENV in the
-     # field 'KAFKA_CONNECT_BOOTSTRAP_SERVERS'
-     brokerport = -999
-     # If you are using a reverse proxy to reach VIPER then you can put it here - otherwise if
-     # empty then no reverse proxy is being used
-     microserviceid = ''
+    replication = 1  # Replication factor for Kafka redundancy
+    numpartitions = 3  # Number of partitions for Kafka topic
+    enabletls = 1  # Enable SSL/TLS communication
+    brokerhost = ''  # Broker host
+    brokerport = -999  # Broker port
+    microserviceid = ''  # Microservice ID if needed
 
-     description = "TML Use Case"
+    description = "TML Use Case"
 
-     # Create the 4 topics in Kafka concurrently - it will return a JSON array
-     result = maadstml.vipercreatetopic(VIPERTOKEN, VIPERHOST, VIPERPORT, maintopic, companyname,
-                                        myname, myemail, mylocation, description, enabletls,
-                                        brokerhost, brokerport, numpartitions, replication,
-                                        microserviceid)
+    # Create the main topic in Kafka
+    result = maadstml.vipercreatetopic(VIPERTOKEN, VIPERHOST, VIPERPORT, maintopic, companyname,
+                                       myname, myemail, mylocation, description, enabletls,
+                                       brokerhost, brokerport, numpartitions, replication,
+                                       microserviceid)
 
-     # Load the JSON array in variable y
-     try:
-         y = json.loads(result, strict='False')
-     except Exception as e:
-         y = json.loads(result)
+    try:
+        y = json.loads(result, strict='False')
+    except Exception as e:
+        y = json.loads(result)
 
-     for p in y:  # Loop through the JSON and grab the topic and producerids
-         pid = p['ProducerId']
-         tn = p['Topic']
+    for p in y:  # Loop through the JSON and grab the topic and producer IDs
+        pid = p['ProducerId']
+        tn = p['Topic']
 
-     result = maadstml.vipercreatetopic(VIPERTOKEN, VIPERHOST, VIPERPORT, preprocesstopic, companyname,
-                                        myname, myemail, mylocation, description, enabletls,
-                                        brokerhost, brokerport, numpartitions, replication,
-                                        microserviceid)
+    # Create the preprocess topic in Kafka
+    result = maadstml.vipercreatetopic(VIPERTOKEN, VIPERHOST, VIPERPORT, preprocesstopic, companyname,
+                                       myname, myemail, mylocation, description, enabletls,
+                                       brokerhost, brokerport, numpartitions, replication,
+                                       microserviceid)
 
-     return tn, pid
+    return tn, pid
 
 
 def sendtransactiondata(maintopic, mainproducerid, VIPERPORT, index, preprocesstopic):
-    #############################################################################################################
-    #                                    PREPROCESS DATA STREAMS
-
     maxrows = 500
     offset = -1
     topic = maintopic
@@ -115,11 +101,9 @@ def sendtransactiondata(maintopic, mainproducerid, VIPERPORT, index, preprocesst
     microserviceid = ''
 
     preprocessconditions = ''
-    
-    # This is where we specify the logic for preprocessing
-    preprocesslogic = 'MIN,MAX,COUNT,VARIANCE,OUTLIERS,ANOMPROB'
+    preprocesslogic = 'MIN,MAX,COUNT,VARIANCE,OUTLIERS,ANOMPROB'  # Define preprocessing logic
 
-    # If your preprocess logic includes certain streams, make sure they are specified here
+    # Define the JSON criteria for the preprocessing
     jsoncriteria = 'uid=metadata.dsn,filter:allrecords~\
     subtopics=metadata.property_name~\
     values=datapoint.value~\
@@ -137,18 +121,26 @@ def sendtransactiondata(maintopic, mainproducerid, VIPERPORT, index, preprocesst
     pathtotmlattrs = 'oem=n/a,lat=n/a,long=n/a,location=n/a,identifier=n/a'
 
     try:
-        # Print debug info to check the preprocess logic
+        # Debug: print the logic being passed
         print(f"Sending preprocessing with logic: {preprocesslogic}")
 
-        # Send the data to preprocess
+        # Send the request to preprocess the data
         result = maadstml.viperpreprocesscustomjson(VIPERTOKEN, VIPERHOST, VIPERPORT, topic, producerid, offset,
                                                     jsoncriteria, 1, maxrows, 1, 70, brokerhost, brokerport,
                                                     microserviceid, -999, streamstojoin, preprocesslogic,
                                                     preprocessconditions, identifier, preprocesstopic, 0, 1, 0, 120,
                                                     usemysql, tmlfilepath, pathtotmlattrs)
 
-        # Print the result to verify if preprocessing was successful
+        # Check and log the result to see if the request was processed successfully
         print(f"Preprocessing result: {result}")
+
+        # Try to interpret the result as JSON if it's valid JSON
+        try:
+            result_json = json.loads(result)
+            print(f"Preprocessing Result JSON: {result_json}")
+        except json.JSONDecodeError:
+            print("Error: Response from VIPER is not a valid JSON")
+        
         return result
 
     except Exception as e:
@@ -157,7 +149,7 @@ def sendtransactiondata(maintopic, mainproducerid, VIPERPORT, index, preprocesst
      
 
 #############################################################################################################
-# SETUP THE TOPIC DATA STREAMS FOR WALMART EXAMPLE
+# SETUP THE TOPIC DATA STREAMS FOR EXAMPLE
 
 maintopic = 'iot-mainstream'
 preprocesstopic = 'iot-preprocess'
@@ -168,16 +160,17 @@ async def startviper():
     print("Start Preprocess-iot-monitor-customdata Request:", datetime.datetime.now())
     while True:
         try:
+            # Call function to send transaction data and start preprocessing
             sendtransactiondata(maintopic, producerid, VIPERPORT, -1, preprocesstopic)
             time.sleep(1)
         except Exception as e:
             print("ERROR:", e)
             continue
-   
 
 async def spawnvipers():
     loop.run_until_complete(startviper())
-  
+
+# Initialize an event loop for asynchronous execution
 loop = asyncio.new_event_loop()
 loop.create_task(spawnvipers())
 asyncio.set_event_loop(loop)
